0da97479edf23a703c97c397e03d1ef537313fc9
Tushar Walavalkar <Tushar.W@WinWire.com>
2023-08-30 01:01:19 -0400

    Implemented read Text file and create Priority Queue.

---
+++ Huffman/Item.cs
@@ -0,0 +1,31 @@
+﻿namespace Huffman
+{
+	public class Item
+	{
+		public Item(byte b)
+		{
+			Value = (char)b;
+			Frequency = 1;
+		}
+
+		public char Value { get; set; }
+		public int Frequency { get; set; }
+		public Item? Next { get; set; }
+		public Item? Previous { get; set; }
+		public Item? Left { get; set; }
+		public Item? Right { get; set; }
+
+		internal void ConsoleWrite(int level, bool newLine)
+		{
+			var quote = level > 0 ? "'" : "";
+			var output = $"{new String(' ', level)}{Value}({Frequency})";
+			if (newLine)
+				Console.WriteLine(output);
+			else
+				Console.Write(output);
+
+			Left?.ConsoleWrite(level + 1, false);
+			Right?.ConsoleWrite(level + 1, true);
+		}
+	}
+}
--- Huffman/Program.cs
+++ Huffman/Program.cs
@@ -2,9 +2,79 @@
 {
 	internal class Program
 	{
+		private static string _sourceFilename = "Source.txt";
+
 		static void Main(string[] args)
 		{
-			
+			// Load text from file
+			var bytes = File.ReadAllBytes(_sourceFilename);
+
+			// Show content
+			Pause("Text read from the file", () =>
+			{
+				bytes.ToList().ForEach(x =>
+				{
+					if (x > 128)
+						return;
+
+					Console.Write((char)x);
+				}
+			);
+			});
+
+			// Create priority queue
+			List<Item> queue = new();
+			bytes.ToList().ForEach(b =>
+			{
+				if (b >= 128)
+					return;
+
+				var existingNode = queue.FirstOrDefault(n => n.Value == (char)b);
+				if (existingNode == null)
+				{
+					queue.Add(new Item(b));
+				}
+				else
+				{
+					existingNode.Frequency++;
+					var index = queue.IndexOf(existingNode);
+					var insertIndex = index - 1;
+					while (insertIndex >= 0)
+					{
+						if (queue[insertIndex].Frequency < existingNode.Frequency)
+							insertIndex--;
+						else
+							break;
+					}
+					if (insertIndex != index)
+					{
+						queue.RemoveAt(index);
+						queue.Insert(insertIndex+1, existingNode);
+					}
+				}
+			}
+			);
+
+			// Show content
+			Pause("Priority Queue", () =>
+			{
+				queue.ForEach(x =>
+				{
+					x.ConsoleWrite(0, true);
+				}
+			);
+			});
+		}
+
+		private static void Pause(string message, Action action)
+		{
+			Console.WriteLine(message + ":");
+			Console.WriteLine(new String('-', message.Length + 1));
+			if (action != null)
+				action();
+			Console.WriteLine();
+			Console.ReadKey();
+			Console.WriteLine();
 		}
 	}
 }
